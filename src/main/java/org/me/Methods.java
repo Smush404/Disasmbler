72c3e5eaf3b8f2b3cf2b8349bc086b5ec1778ecd
Haakon H <hhaus532@iastate.edu>
2023-11-25 13:38:37 -0600

    started building the hasmap of instrustion

--- src/main/java/org/me/Methods.java
+++ src/main/java/org/me/Methods.java
@@ -2,6 +2,8 @@ package org.me;
 
 import java.io.*;
 import java.util.BitSet;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Methods {
@@ -9,6 +11,61 @@ public class Methods {
     private File inputfile = null;
     private InputStream inputStream = null;
 
+    private HashMap<String, BitSet> instruction = new HashMap<>();
+      { "ADD",   10001011000 },
+      { "ADDI",  1001000100 },
+      { "ADDIS", 1011000100  },
+      { "ADDS",  10101011000 },
+      { "AND",   10001010000 },
+      { "ANDI",  1001001000  },
+      { "ANDIS", 1111001000  },
+      { "ANDS",  1110101000  },
+      { "B",     000101  },
+      { "BL",    100101      },
+      { "BR",    11010110000 },
+      { "CBNZ",  10110101    },
+      { "CBZ",   10110100    },
+      { "DUMP",  11111111110 },
+      { "EOR",   11001010000 },
+      { "EORI",  1101001000  },
+      { "FADDD", 0b00011110011 },
+      { "FADDS", 00011110001 },
+      { "FCMPD", 00011110011 },
+      { "FCMPS", 00011110001 },
+      { "FDIVD", 00011110011 },
+      { "FDIVS", 00011110001 },
+      { "FMULD", 00011110011 },
+      { "FMULS", 00011110001 },
+      { "FSUBD", 00011110011 },
+      { "FSUBS", 00011110001 },
+      { "HALT",  11111111111 },
+      { "LDUR",  11111000010 },
+      { "LDURB", 00111000010 },
+      { "LDURD", 11111100010 },
+      { "LDURH", 01111000010 },
+      { "LDURS", 10111100010 },
+      { "LDURSW",10111000100 },
+      { "LSL",   11010011011 },
+      { "LSR",   11010011010 },
+      { "MUL",   10011011000 },
+      { "ORR",   10101010000 },
+      { "ORRI",  1011001000  },
+      { "PRNL",  11111111100 },
+      { "PRNT",  11111111101 },
+      { "SDIV",  10011010110 },
+      { "SMULH", 10011011010 },
+      { "STUR",  11111000000 },
+      { "STURB", 00111000000 },
+      { "STURD", 11111100000 },
+      { "STURH", 01111000000 },
+      { "STURS", 10111100000 },
+      { "STURSW",10111000000 },
+      { "SUB",   11001011000 },
+      { "SUBI",  1101000100  },
+      { "SUBIS", 1111000100  },
+      { "SUBS",  11101011000 },
+      { "UDIV",  10011010110 },
+      { "UMULH", 10011011110 }
 
     public BitSet byteToBit(byte[] bytelist){
         BitSet bitSet = BitSet.valueOf(bytelist);
@@ -24,6 +81,13 @@ public class Methods {
     public Methods(File f) throws FileNotFoundException {
         inputfile = f;
         inputStream = new FileInputStream(f);
+        buildHash();
+    }
+
+    private void buildHash() {
+          BitSet b = new BitSet();
+          for(int i = 0; i < "10001011000".length(); i++){ if()
+          instruction.put("ADD", 10001011000);
     }
 
     protected byte[] reader() throws IOException {
@@ -56,6 +120,8 @@ public class Methods {
      * @return String that gives the type of op
      */
     public String getType(BitSet bitSet) {
+
+
         return "R - ADDI";
     }
 }
