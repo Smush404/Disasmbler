47c5906efd8516e15867d20cb5e626f2fc88aee2
Haakon H <hhaus532@iastate.edu>
2023-11-27 21:41:02 -0600

    2 Arrays

--- src/main/java/org/me/Methods.java
+++ src/main/java/org/me/Methods.java
@@ -1,6 +1,8 @@
 package org.me;
 
 import java.io.*;
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.HashMap;
 import java.util.Map;
@@ -11,61 +13,9 @@ public class Methods {
     private File inputfile = null;
     private InputStream inputStream = null;
 
-    private HashMap<String, BitSet> instruction = new HashMap<>();
-//      { "ADD",   10001011000 },
-//      { "ADDI",  1001000100  },
-//      { "ADDIS", 1011000100  },
-//      { "ADDS",  10101011000 },
-//      { "AND",   10001010000 },
-//      { "ANDI",  1001001000  },
-//      { "ANDIS", 1111001000  },
-//      { "ANDS",  1110101000  },
-//      { "B",     000101      },
-//      { "BL",    100101      },
-//      { "BR",    11010110000 },
-//      { "CBNZ",  10110101    },
-//      { "CBZ",   10110100    },
-//      { "DUMP",  11111111110 },
-//      { "EOR",   11001010000 },
-//      { "EORI",  1101001000  },
-//      { "FADDD", 0b00011110011 },
-//      { "FADDS", 00011110001 },
-//      { "FCMPD", 00011110011 },
-//      { "FCMPS", 00011110001 },
-//      { "FDIVD", 00011110011 },
-//      { "FDIVS", 00011110001 },
-//      { "FMULD", 00011110011 },
-//      { "FMULS", 00011110001 },
-//      { "FSUBD", 00011110011 },
-//      { "FSUBS", 00011110001 },
-//      { "HALT",  11111111111 },
-//      { "LDUR",  11111000010 },
-//      { "LDURB", 00111000010 },
-//      { "LDURD", 11111100010 },
-//      { "LDURH", 01111000010 },
-//      { "LDURS", 10111100010 },
-//      { "LDURSW",10111000100 },
-//      { "LSL",   11010011011 },
-//      { "LSR",   11010011010 },
-//      { "MUL",   10011011000 },
-//      { "ORR",   10101010000 },
-//      { "ORRI",  1011001000  },
-//      { "PRNL",  11111111100 },
-//      { "PRNT",  11111111101 },
-//      { "SDIV",  10011010110 },
-//      { "SMULH", 10011011010 },
-//      { "STUR",  11111000000 },
-//      { "STURB", 00111000000 },
-//      { "STURD", 11111100000 },
-//      { "STURH", 01111000000 },
-//      { "STURS", 10111100000 },
-//      { "STURSW",10111000000 },
-//      { "SUB",   11001011000 },
-//      { "SUBI",  1101000100  },
-//      { "SUBIS", 1111000100  },
-//      { "SUBS",  11101011000 },
-//      { "UDIV",  10011010110 },
-//      { "UMULH", 10011011110 }
+    private ArrayList<String> opNames = new ArrayList<>();
+    private ArrayList<Long> opCodes = new ArrayList<>();
+
 
     public BitSet byteToBit(byte[] bytelist){
         BitSet bitSet = BitSet.valueOf(bytelist);
@@ -81,14 +31,118 @@ public class Methods {
     public Methods(File f) throws FileNotFoundException {
         inputfile = f;
         inputStream = new FileInputStream(f);
-        buildHash();
+        buildArrays();
     }
 
-    private void buildHash() {
-          BitSet b = new BitSet();
-          for(int i = 0; i < "10001011000".length(); i++){}
-          instruction.put("ADD", 10001011000);
+    private void buildArrays() {
+        opNames.add("AND");
+        opNames.add("ANDIS");
+        opNames.add("ANDS");
+        opNames.add("B");
+        opNames.add("BL");
+        opNames.add("BR");
+        opNames.add("CBNZ");
+        opNames.add("CBZ");
+        opNames.add("DUMP");
+        opNames.add("EOR");
+        opNames.add("EORI");
+        opNames.add("FADDD");
+        opNames.add("FADDS");
+        opNames.add("FCMPD");
+        opNames.add("FCMPS");
+        opNames.add("ANDI");
+        opNames.add("FDIVD");
+        opNames.add("FDIVS");
+        opNames.add("FMULD");
+        opNames.add("FMULS");
+        opNames.add("FSUBD");
+        opNames.add("FSUBS");
+        opNames.add("HALT");
+        opNames.add("LDUR");
+        opNames.add("LDURB");
+        opNames.add("LDURD");
+        opNames.add("LDURH");
+        opNames.add("LDURS");
+        opNames.add("LDURSW");
+        opNames.add("LSL");
+        opNames.add("LSR");
+        opNames.add("MUL");
+        opNames.add("ORR");
+        opNames.add("ORRI");
+        opNames.add("PRNL");
+        opNames.add("PRNT");
+        opNames.add("SDIV");
+        opNames.add("SMULH");
+        opNames.add("STUR");
+        opNames.add("STURB");
+        opNames.add("STURD");
+        opNames.add("STURH");
+        opNames.add("STURS");
+        opNames.add("STURSW");
+        opNames.add("SUB");
+        opNames.add("SUBI");
+        opNames.add("SUBIS");
+        opNames.add("SUBS");
+        opNames.add("UDIV");
+        opNames.add("UMULH");
+
+        opCodes.add(10001011000L);
+        opCodes.add(1001000100L);
+        opCodes.add(1011000100L);
+        opCodes.add(10101011000L);
+        opCodes.add(10001010000L);
+        opCodes.add(1001001000L);
+        opCodes.add(1111001000L);
+        opCodes.add(1110101000L);
+        opCodes.add(000101L);
+        opCodes.add(100101L);
+        opCodes.add(11010110000L);
+        opCodes.add(10110101L);
+        opCodes.add(10110100L);
+        opCodes.add(11111111110L);
+        opCodes.add(11001010000L);
+        opCodes.add(1101001000L);
+        opCodes.add(0b00011110011L);
+        opCodes.add(00011110001L);
+        opCodes.add(00011110011L);
+        opCodes.add(00011110001L);
+        opCodes.add(00011110011L);
+        opCodes.add(00011110001L);
+        opCodes.add(00011110011L);
+        opCodes.add(00011110001L);
+        opCodes.add(00011110011L);
+        opCodes.add(00011110001L);
+        opCodes.add(11111111111L);
+        opCodes.add(11111000010L);
+        opCodes.add(00111000010L);
+        opCodes.add(11111100010L);
+        opCodes.add(01111000010L);
+        opCodes.add(10111100010L);
+        opCodes.add(10111000100L);
+        opCodes.add(11010011011L);
+        opCodes.add(11010011010L);
+        opCodes.add(10011011000L);
+        opCodes.add(10101010000L);
+        opCodes.add(1011001000L);
+        opCodes.add(11111111100L);
+        opCodes.add(11111111101L);
+        opCodes.add(10011010110L);
+        opCodes.add(10011011010L);
+        opCodes.add(11111000000L);
+        opCodes.add(00111000000L);
+        opCodes.add(11111100000L);
+        opCodes.add(01111000000L);
+        opCodes.add(10111100000L);
+        opCodes.add(10111000000L);
+        opCodes.add(11001011000L);
+        opCodes.add(1101000100L);
+        opCodes.add(1111000100L);
+        opCodes.add(11101011000L);
+        opCodes.add(10011010110L);
+        opCodes.add(10011011110L);
     }
+
+
 
     protected byte[] reader() throws IOException {
 
